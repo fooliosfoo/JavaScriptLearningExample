d363490ca0df3be7b897e03efa8710d30f65d0df
fooliosfoo <gnikollaj@gmail.com>
2017-01-26 22:13:52 -0600

    Update extJS.js

--- extJS.js
+++ extJS.js
@@ -1,2 +1,24 @@
 /* TEMP */
-code to follow
+
+/* this application validates a user's entries for joining
+*/
+var$~ function(id ) { II the standard$ function
+return document.getElementByid(id);}
+/* this function gets and validates the user entries
+*/
+var joinList ~ function( ) {
+var emailAddressl ~ $ ( "email_addressl") .value;
+var emai1Address2 ~ $ ( "email_address2") .value;
+var firstName ~ $ ( "first_name") .value;
+var isValid ~ true;
+/* validate the first entr
+*/
+if (emailAddressl ~~ "" ) {
+$ ( "email_addressl_error" ) .first Child .nodeValue ~
+"This field is required.";
+isValid ~ false; /I set valid switch to off
+} else {
+$("email_addressl_error" ) .firstChild.nodeValue ~
+}
+/* validate the second entry
+*/
