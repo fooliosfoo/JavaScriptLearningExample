644f74ec9a116b8306449b4a326c85b725e38d3a
fooliosfoo <gnikollaj@gmail.com>
2017-01-27 22:06:03 -0600

    Update extJS.js

--- extJS.js
+++ extJS.js
@@ -5,7 +5,7 @@
 //Examples
 //print();
 //alert(window.location); // Displays the URL of the current paqe
-var entryA = prompt("Enter any value");
+var entryA = prompt("Enter any value", 12345.66);
 alert(entryA);
 entryA = parseInt(entryA);
 alert(entryA);
