a6bbd7f874d5b9aa766ae64432cf6579edbeaff6
fooliosfoo <gnikollaj@gmail.com>
2017-02-02 18:25:37 -0600

    Update timer.js

--- timer.js
+++ timer.js
@@ -62,5 +62,5 @@ function round(value, decimals) {
   return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
 }
 function khFunction() {
-  window.alert ( "khFunction fired because the kh value field was clicked onto" ) ;
+  //window.alert ( "khFunction fired because the kh value field was altered" ) ;
 }
